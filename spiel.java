20958fa761c25f458d3c97ebed975ad0e97ec23a
TofTof1 <dracheover9000@gmail.com>
2023-04-03 19:41:52 +0200

    .

--- spiel.java
+++ spiel.java
@@ -1,7 +1,7 @@
 public class spiel{
     static Fabrik fabrik = new Fabrik();
     static int anzahlSpieler = 5; // Ändern
-    static int anzahlKunden = 100;
+    static int anzahlKunden = 101;
     static Spieler[] spielerListe = new Spieler[anzahlSpieler];
     public static void main(String[] args)
     {
