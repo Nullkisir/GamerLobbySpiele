38097338751676a8d9ec6499cb942902219b3185
TofTof1 <dracheover9000@gmail.com>
2023-04-03 18:44:30 +0200

    test

--- ereigniskarten.java
+++ ereigniskarten.java
@@ -1,4 +1,41 @@
 public class ereigniskarten
 {
+    String[] ereignissTexte = {"Test0","Test1"};
+    int anzahlEreignisse = 4; //Anzahl muss geändert werden
+    public void ereigniskartenMain()
+    {
+        double wahrscheinlichkeitFuerEreigniss = Math.random();
+        //Überprüft ob ein Ereigniss eintritt
+        if(wahrscheinlichkeitFuerEreigniss <= 0.33)
+        {
+            double indexEreigniss = Math.random() * (anzahlEreignisse-1);
 
+            //Ereignisse:
+            if(indexEreigniss == 0)
+            {
+                ausgabe("Test0");
+            }
+            else if(indexEreigniss == 1)
+            {
+                ausgabe("Test1");
+            }
+            else if(indexEreigniss == 2)
+            {
+                ausgabe("Test2");
+            }
+            else if(indexEreigniss == 3)
+            {
+                ausgabe("Test3");
+            }
+        }
+    }
+    //Funktion regelt die Ausgabe:
+    private void ausgabe(String text)
+    {
+        System.out.println(text);
+    }
+    private int indexAusgabeZuTextEingabe()
+    {
+        return 0;
+    }
 }
