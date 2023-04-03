0a7dd55d8af190d2e01457cd474f7621edc64a22
TofTof1 <dracheover9000@gmail.com>
2023-04-03 20:15:14 +0200

    Spiel anpassung

--- spiel.java
+++ spiel.java
@@ -2,8 +2,23 @@ public class spiel{
     static Fabrik fabrik = new Fabrik();
     static int anzahlSpieler = 5; // Ändern
     static int anzahlKunden = 101;
+    static boolean spielEnde = false;
     static Spieler[] spielerListe = new Spieler[anzahlSpieler];
     public static void main(String[] args)
+    {
+        spielerAnlegen();
+        while(!spielEnde)
+        {
+            spielAbblauf();
+        }
+    }
+    //Legt die Anzahl und Namen der Spieler fest und erstellt sie
+    private static void spielerAnlegen()
+    {
+
+    }
+    //Hier findet der komplette Ablauf einer Runde statt wird aufgerufen bis spielEnde = false
+    private static void spielAbblauf()
     {
         fabrik.produktionskosten();
     }
