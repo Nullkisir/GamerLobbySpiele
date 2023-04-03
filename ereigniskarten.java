9ad1fbfa1cee20bcc19551567673e9f0d9983a3a
TofTof1 <dracheover9000@gmail.com>
2023-04-03 19:33:42 +0200

    ereigniskarten v1

--- ereigniskarten.java
+++ ereigniskarten.java
@@ -1,31 +1,57 @@
 public class ereigniskarten
 {
-    String[] ereignissTexte = {"Test0","Test1"};
-    int anzahlEreignisse = 4; //Anzahl muss geändert werden
+    String[] ereignissTexte =
+            {       "Senkt die Produktionskosten für eine Runde um 5 bis 25%",
+                    "Steigert die Produktionskosten für eine Runde um 5 bis 25%",
+                    "Senkt die Anzahl der Kunden um 10 bis 50 Kunden",
+                    "Steigert die Anzahl der Kunden um 10 bis 50 Kunden "};
+    int anzahlEreignisse = ereignissTexte.length;
     public void ereigniskartenMain()
     {
         double wahrscheinlichkeitFuerEreigniss = Math.random();
         //Überprüft ob ein Ereigniss eintritt
         if(wahrscheinlichkeitFuerEreigniss <= 0.33)
         {
-            double indexEreigniss = Math.random() * (anzahlEreignisse-1);
+            int indexEreigniss = (int) (Math.random() * (anzahlEreignisse-1));
 
             //Ereignisse:
             if(indexEreigniss == 0)
             {
-                ausgabe("Test0");
+                ausgabe(ereignissTexte[0]);
+
+                //Änderung: Produktionskosten Senkung 5 bis 25% eine Runde
+                double produktionsKostenSenkung = (int) (Math.random() * 20 + 5);
+                for(int i = 0; i < spiel.spielerListe.length; i ++)
+                {
+                    spiel.spielerListe[i].fabrik.produktionsPreis = spiel.spielerListe[i].fabrik.produktionsPreis * (1 - produktionsKostenSenkung/100);
+                }
             }
             else if(indexEreigniss == 1)
             {
-                ausgabe("Test1");
+                ausgabe(ereignissTexte[1]);
+
+                //Änderung: Produktionskosten Steigung 5 bis 25% eine Runde
+                double produktionsKostenSteigung = (int) (Math.random() * 20 + 5);
+                for(int i = 0; i < spiel.spielerListe.length; i ++)
+                {
+                    spiel.spielerListe[i].fabrik.produktionsPreis = spiel.spielerListe[i].fabrik.produktionsPreis * (1 + produktionsKostenSteigung/100);
+                }
             }
             else if(indexEreigniss == 2)
             {
-                ausgabe("Test2");
+                ausgabe(ereignissTexte[2]);
+
+                //Änderung: Anzahl Kunden verringert um 10 bis 50 eine Runde
+                int anzahlKundenSenkung = (int) (Math.random() * 40 + 10);
+                spiel.anzahlKunden = spiel.anzahlKunden - anzahlKundenSenkung;
             }
             else if(indexEreigniss == 3)
             {
-                ausgabe("Test3");
+                ausgabe(ereignissTexte[3]);
+
+                //Änderung: Anzahl Kunden steigt um 10 bis 50 eine Runde
+                int anzahlKundenSteigung = (int) (Math.random() * 40 + 10);
+                spiel.anzahlKunden = spiel.anzahlKunden + anzahlKundenSteigung;
             }
         }
     }
@@ -34,8 +60,16 @@ public class ereigniskarten
     {
         System.out.println(text);
     }
-    private int indexAusgabeZuTextEingabe()
+    //Funktion findet zu einer Text Eingabe den Index des Arrays. Wenn nicht vorhanden dann return -1
+    private int indexAusgabeZuTextEingabe(String text)
     {
-        return 0;
+        for(int i = 0; i < ereignissTexte.length; i++)
+        {
+            if(ereignissTexte[i].equals(text))
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
