c09816481ed90e3ff4c78b0c2816ce3c3ce4e890
Nullkisir <kevin.scheurer01@gmx.de>
2023-04-04 13:05:53 +0200

    test

---
+++ SpielerKarten.java
@@ -0,0 +1,18 @@
+public class SpielerKarten {
+
+    /*@ TODO Camo wie wir das hatten, ka wie ob das geht.
+        TODO Man hat Karten mit Aktionen drauf, aehnlich wie die Ereignisse
+        TODO Man muss die dann nach der Eingabe spielen und aendert so den Spielverlauf
+     */
+    /*  Moegliche Karten:
+        -Nix da
+        -Mehr Kunden
+        -Keine Kunden
+        -Kein Ereignis diese Runde
+        -10 Produkte von dir werden safe verkauft
+        -maybe sowas wie eine Sabotage aber ka wie man das machen kann
+        TODO man muss dann halt noch gucken wie man Nix da o.ae. einbauen kann, da dies ja Reaktionen sind
+     */
+
+
+}
