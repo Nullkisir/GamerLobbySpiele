79a2829285d4245b7402e067869e7114a78250fa
Nullkisir <kevin.scheurer01@gmx.de>
2023-04-04 09:43:05 +0200

    test

--- Markt.java
+++ Markt.java
@@ -13,3 +13,6 @@ public class Markt {
         scan.close();
     }
 }
+//evtl. kann man noch Versicherungen einbauen, die man pro Runde bezahlen muss.
+//man kann dann iwo Sachen machen die passieren koennen und wenn man keine Versicherung hat dann muss man zahlen
+//Die Versicherung waere dann pro Runde zu bezahlen
